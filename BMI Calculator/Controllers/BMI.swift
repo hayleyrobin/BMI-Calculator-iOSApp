7255da45400176472970d1f48689f72d061f11c7
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2022-01-11 14:50:18 -0500

    Implemented new screen using segue

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Hayley Robinson on 1/11/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
